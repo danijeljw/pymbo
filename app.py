f2e39e3a37ab0615fc409dd017c05e75b98577cb
Danijel-James Wynyard <danijel.james@me.com>
2018-02-23 13:37:41 +1100

    pymbo main app file

---
+++ app.py
@@ -0,0 +1,67 @@
+app = Flask(__name__)
+
+#Facebook Messenger API credentials from keys
+ACCESS_TOKEN = fb_keys['ACCESS_TOKEN']
+VERIFY_TOKEN = fb_keys['VERIFY_TOKEN']
+
+bot = Bot(ACCESS_TOKEN)
+
+#We will receive messages that Facebook sends our bot at this endpoint 
+@app.route("/", methods=['GET', 'POST'])
+def receive_message():
+    if request.method == 'GET':
+        """Before allowing people to message your bot, Facebook has implemented a verify token
+        that confirms all requests that your bot receives came from Facebook.""" 
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
+    else:
+        # get whatever message a user sent the bot
+       output = request.get_json()
+       for event in output['entry']:
+          messaging = event['messaging']
+          for message in messaging:
+            if message.get('message'):
+                # Facebook Messenger ID for user so we know where to send response back to
+                recipient_id = message['sender']['id']
+                if message['message'].get('text'):
+                    response_sent_text = get_message()
+                    send_message(recipient_id, response_sent_text)
+                # if user sends us a GIF, photo,video, or any other non-text item
+                if message['message'].get('attachments'):
+                    response_sent_nontext = get_message()
+                    send_message(recipient_id, response_sent_nontext)
+    return "Message Processed"
+
+
+def verify_fb_token(token_sent):
+    """Take token sent by facebook and verify it matches the verify token you sent
+    if they match, allow the request, else return an error"""
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'
+
+"""
+def get_message()
+
+Need to improve this function. At the moment it's only sending a reply to users
+from a random selection in a list provided by 'sample_responses' which works
+for the time being, but need to incorporate a kind of AI into this or elevated
+responses to the user.
+"""
+
+
+# Chooses a random message to send to the user
+def get_message():
+    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
+    # return selected item to the user
+    return random.choice(sample_responses)
+
+# Uses PyMessenger to send response to user
+def send_message(recipient_id, response):
+    # Sends user the text message provided via input response parameter
+    bot.send_text_message(recipient_id, response)
+    return "success"
+
+if __name__ == "__main__":
+    app.run()
